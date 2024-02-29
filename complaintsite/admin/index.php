b9d428b1a34d3d8984a2663ced024d71093f9bdc
karuna-ck <133120406+karuna-ck@users.noreply.github.com>
2024-02-29 13:52:44 +0530

    updated index

--- complaintsite/admin/index.php
+++ complaintsite/admin/index.php
@@ -35,7 +35,7 @@ exit();
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Complaint Site | Admin login</title>
+	<title>Complaint Site | Admin in</title>
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="css/theme.css" rel="stylesheet">
